ea648614be248a246169aee27c57966a670760e5
Franck van Breugel <franck@cs.yorku.ca>
2020-02-18 16:06:28 -0500

    added license info and Javadoc

--- code/Formula.java
+++ code/Formula.java
@@ -1,5 +1,32 @@
+/*
+ * Copyright (C)  2020  Amgad Rady and Franck van Breugel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
+/**
+ * A formula.
+ *
+ * @author Amgad Rady
+ * @author Franck van Breugel
+ */
 public abstract class Formula {
-
-	public abstract String toLaTeX();
+    
+    /**
+     * Returns a LaTeX representation of this formula.
+     *
+     * @return a LaTeX representation of this formula
+     */	
+    public abstract String toLaTeX();
 }
