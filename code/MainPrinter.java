15e3379a5a2dd1c83095a74329e0cbf47426e144
amgad-rady <amgad.s.rady@gmail.com>
2020-04-08 10:46:20 -0400

    Performance improvement in computing distance matrix

--- code/MainPrinter.java
+++ code/MainPrinter.java
@@ -11,18 +11,19 @@ class MainPrinter {
    * @param n   number of iterations
    * @param s   first state
    * @param t   second state
-   * @return
+   * @return string containing stepwise separating formulas
    */
   public static String print(LabelledMarkovChain lmc, int n, int s, int t) {
     double[][] probabilities = lmc.probability;
     int[] labels = lmc.label;
     int states = lmc.label.length;
-    double[][] distances = new double[states][states];
+    double[][] distances = lmc.distance;
+    double[][] intermediate_distances = new double[states][states];
 
     for (int u = 0; u < states; u++) {
       for (int v = 0; v < states; v++) {
         if (labels[u] != labels[v]) {
-          distances[u][v] = 1.0;
+          intermediate_distances[u][v] = 1.0;
         }
       }
     }
@@ -34,20 +35,22 @@ class MainPrinter {
       /*
       Compute the KR dual
        */
-      KR_dual = (new KRDualSolver(distances, probabilities, states, s, t)).getKRdual();
+      KR_dual = (new KRDualSolver(intermediate_distances, probabilities, states, s, t)).getKRdual();
       output.append(FormulaPrinter.formula_generator(s, t, n, states, labels,
-        KR_dual, distances, probabilities).toLaTeX());
+        KR_dual, intermediate_distances, probabilities).toLaTeX());
 
       /*
-      Compute the next distance matrix pointwise
+      Compute the next distance matrix point-wise
        */
-      double[][] new_distances = distances.clone();
+      double[][] new_distances = intermediate_distances.clone();
       for (int u = 0; u < states; u++) {
         for (int v = 0; v < states; v++) {
-          new_distances[u][v] = (new OptimalCouplingComputer(u, v, probabilities, distances)).compute_distance();
+          if (distances[u][v] != 0 && labels[u] == labels[v]) {
+            new_distances[u][v] = (new OptimalCouplingComputer(u, v, probabilities, intermediate_distances)).compute_distance();
+          }
         }
       }
-      distances = new_distances;
+      intermediate_distances = new_distances;
     }
 
     return output.toString();
